8fc5f2512103726b13000acf200e668416e1127b
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-06-22 14:25:57 -0400

    started the header and main

---
+++ vite-foundation-site/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "../assets/logo.jpeg"
+
+const Header = () => {
+    
+    return(
+            <img src={logo} className="h-25 w-40"></img>
+    )
+}
+
+export default Header
---
+++ vite-foundation-site/src/components/Main.tsx
@@ -0,0 +1,11 @@
+const Main = () => {
+    return(
+        <>
+            <h1>Hello There</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste et deserunt minima magni illum ex, omnis maxime aspernatur nulla exercitationem?</p>
+
+        </>
+    )
+}
+
+export default Main
