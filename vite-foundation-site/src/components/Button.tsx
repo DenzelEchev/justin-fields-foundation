f6655ded7c6ab132df08fdc69cbe608b795bf4f6
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-07-13 04:50:06 -0400

    added hover to buttons

--- vite-foundation-site/src/components/Button.tsx
+++ vite-foundation-site/src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = ({bName}:{bName: string}) => {
     return (
-        <button className="self-center w-1/5 text-white border-2 border-white border-solid rounded"> { bName } </button>
+        <button className="self-center w-1/5 text-white duration-300 border-2 border-white border-solid rounded hover:bg-white hover:text-black hover:ease-in"> { bName } </button>
     )
 }
 
