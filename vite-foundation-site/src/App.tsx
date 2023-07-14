86c93bcbecad11b62e8699a698f95599f27c8e56
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-07-14 15:39:50 -0400

    lite changes

--- vite-foundation-site/src/App.tsx
+++ vite-foundation-site/src/App.tsx
@@ -8,20 +8,24 @@ import Button from './components/Button'
 const App = () => {
   return(
     <>
-        <header className="sticky top-0 flex justify-center p-2 bg-white">
-          <Header />
-        </header>
+      <header className="sticky top-0 flex justify-center p-2 bg-white">
+        <Header />
+      </header>
+
       <main className="flex flex-col h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
         <Main />
         <Button bName={ "About Us" }/>
       </main>
+
       <section className='flex flex-col p-8 lg:flex-row'>
         <Story />
       </section>
+
       <section className="flex flex-col content-center px-8 py-24 bg-gradient-to-r from-indigo-500 to-sky-500">
         <Donations />
         <Button bName={ "Donate" }/>
       </section>
+      
       <footer className="flex justify-center p-2 py-2">
         <Footer />
       </footer>
